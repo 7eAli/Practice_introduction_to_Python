a3d71cd44dc3be92abc2dc42ccce18b6e8db2e24
GSY <gle722@yandex.ru>
2023-04-08 11:57:34 +0300

    Решил задание 5

--- Lesson 1/Task_5.py
+++ Lesson 1/Task_5.py
@@ -1,3 +1,15 @@
 # Напишите программу, которая находит наибольшее и
 # наименьшее число из списка значений
 
+numbers = [1, 6, 9, 10, -3, -4, 5]
+max = numbers[0]
+min = numbers[1]
+print(numbers)
+for i in range(2, len(numbers)):
+    if numbers[i] > max:
+        max = numbers[i]
+    elif numbers[i] < min:
+        min = numbers[i]
+
+print(f"max = {max}")
+print(f"min = {min}")
