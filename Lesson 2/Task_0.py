f00d81519b8f99e6d2ad9d65d71fc94ab8a998b2
GSY <gle722@yandex.ru>
2023-04-09 10:37:53 +0300

    Преобразовал демонстрационное задание в метод

--- Lesson 2/Task_0.py
+++ Lesson 2/Task_0.py
@@ -1,18 +1,18 @@
 # Дано число N. Найти все его делители. Для
 # каждого делителя укажите чётный он или нечётный.
+def Task_1():
+    number = int(
+                input("Введите число: "))
 
-number = int(
-            input("Введите число: "))
-
-for i in range(1, number + 1):
-    if number % i == 0:
-        if i % 2 == 0:
-            if i != number:
-                print(f"{i}, четный", end="; ")
+    for i in range(1, number + 1):
+        if number % i == 0:
+            if i % 2 == 0:
+                if i != number:
+                    print(f"{i}, четный", end="; ")
+                else:
+                    print(f"{i}, четный")
             else:
-                print(f"{i}, четный")
-        else:
-            if i != number:
-                print(f"{i}, нечетный", end="; ")
-            else:
-                print(f"{i}, нечетный.")
+                if i != number:
+                    print(f"{i}, нечетный", end="; ")
+                else:
+                    print(f"{i}, нечетный.")
