fccddf5673aa4616d8664d9a45b3f05bc411a17b
GSY <gle722@yandex.ru>
2023-04-09 10:52:44 +0300

    Решил задание 1

--- Lesson 2/Tasks.py
+++ Lesson 2/Tasks.py
@@ -1,6 +1,6 @@
 # Дано число N. Найти все его делители. Для
 # каждого делителя укажите чётный он или нечётный.
-def Task_1():
+def Task_0():
     number = int(
                 input("Введите число: "))
 
@@ -16,3 +16,26 @@ def Task_1():
                     print(f"{i}, нечетный", end="; ")
                 else:
                     print(f"{i}, нечетный.")
+
+# Task_0()
+
+
+# Выведите таблицу истинности для
+# выражения ¬ X ∨ Y.
+
+def Task_1():    
+    for x in range(2):
+        for y in range(2):
+            print(f"{x} {y}", end=" ")
+            if not x or y:
+                print(1)
+            else:
+                print(0)
+
+Task_1()
+
+
+
+# Task_2()
+# Task_3()
+# Task_4()
