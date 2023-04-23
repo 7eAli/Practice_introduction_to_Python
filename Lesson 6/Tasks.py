d7f24ee08a674114174378640d209d53b6ebb197
GSY <gle722@yandex.ru>
2023-04-23 10:01:48 +0300

    Подготовка файла для семинара 6

---
+++ Lesson 6/Tasks.py
@@ -0,0 +1,20 @@
+
+
+flag = True
+while flag:
+    print()
+    choice = input("Введите номер задания или нажмите q для выхода: ")
+    if choice == "0":
+        Task_0()
+    elif choice == "1":
+        Task_1()
+    elif choice == "2":
+        Task_2()
+    # elif choice == "3":
+    #     Task_3()
+    # elif choice == "4":
+    #     Task_4()
+    elif choice == "q":
+        flag = False
+    else:
+        print("Нет такого задания")
