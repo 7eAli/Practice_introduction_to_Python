213b97254eaff99b0a11893f3338fcbc29825665
GSY <gle722@yandex.ru>
2023-04-23 10:07:46 +0300

    Решил задачу 1

--- Lesson 6/Tasks.py
+++ Lesson 6/Tasks.py
@@ -1,17 +1,29 @@
+import random
+
+# Задача 1. Дан список случайных элементов.
+# Проверьте, что все его элементы уникальны.
+def Task_1():
+    numbers = [random.randint(1, 100) for i in range(10)]
+    check = set(numbers)
+    print(numbers)
+    if len(numbers) == len(check):
+        print("Да")
+    else:
+        print("Нет")
+
+
 
 
 flag = True
 while flag:
     print()
     choice = input("Введите номер задания или нажмите q для выхода: ")
-    if choice == "0":
-        Task_0()
-    elif choice == "1":
+    if choice == "1":
         Task_1()
     elif choice == "2":
         Task_2()
-    # elif choice == "3":
-    #     Task_3()
+    elif choice == "3":
+        Task_3()
     # elif choice == "4":
     #     Task_4()
     elif choice == "q":
