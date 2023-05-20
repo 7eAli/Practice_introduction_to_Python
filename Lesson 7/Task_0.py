13104d8c0948eaac640407b1bea46adc47af0a6f
GSY <gle722@yandex.ru>
2023-05-20 10:29:48 +0300

    Решили задачу 1

---
+++ Lesson 7/Task_0.py
@@ -0,0 +1,8 @@
+def our_filter(func, numbers):
+    return (el for el in numbers if func(el))
+
+numbers = [1, 14, 6, 10, 3, 2, 5]
+
+#print( list(filter(lambda x: x > 5, numbers)))
+print( list(our_filter(lambda x: x > 5, numbers)))
+print(type(lambda x: x > 5))
