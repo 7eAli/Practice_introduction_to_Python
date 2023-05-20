ff19ca75867369d580e21a20ceb87b7c9d07e7cd
GSY <gle722@yandex.ru>
2023-05-20 12:35:05 +0300

    Дополнили задачу 5

--- Lesson 7/Task_4.py
+++ Lesson 7/Task_4.py
@@ -1,4 +1,5 @@
 import telebot
+import requests
 
 bot = telebot.TeleBot("6237928184:AAGL69utLeIJe_jZxqI_ecUf8ccFkLq1dU0")
 
@@ -11,5 +12,10 @@ def handle_docs_audio(message):
     text = message.text
     if 'привет' in text:
         bot.reply_to(message, f'Привет, {message.from_user.first_name}')
-
+    elif text == 'погода':
+        req = requests.get('https://wttr.in/?0T')
+        bot.reply_to(message, req.text)
+    elif text == 'котик':
+        req = requests.get('https://cataas.com/cat/gif')
+        bot.send_photo(message.from_user.id, req.content)
 bot.polling()
